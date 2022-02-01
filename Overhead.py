850360d696d2f981b2c00fb9daeebc9ed2647c75
Gary J Davis <jonathan.davis1927@yahoo.com>
2022-01-31 21:36:54 -0600

    Add files via upload

--- Overhead.py
+++ Overhead.py
@@ -12,21 +12,25 @@
 #Start defining programs
 from re import U
 from AddToRecordsEdit import AddToRecords 
+def remove(string):
+    return string.replace(" ","")
+
 currentusername = input("Please enter your username:")
 print("--------")
 print("Processing")
 print("--------")
-with open("C:/Users/davisgj/Desktop/Python Files/Employee Time Project/EmployeeProject/Passwords.txt", "r") as u:
-    lines = u.readlines()
+with open("C:/Users/davisgj/Desktop/Python Files/Employee Time Project/EmployeeProject/Passwords.txt", "r") as passdatabase:
+    lines = passdatabase.readlines()
     founditflag = 0
     #iterate through each line of the list searching for the name that you want to change
     for line in lines:
         #found it!
         if(line.__contains__(currentusername)):
             print("username found")
+            passlinewhole = line
             passline = line[11:15]
             founditflag = 1
-            u.close()
+            passdatabase.close()
     if (founditflag == 0):
         print("Username not found in database")
         AddToRecords(currentusername)
@@ -34,7 +38,45 @@ with open("C:/Users/davisgj/Desktop/Python Files/Employee Time Project/EmployeeP
         passwordinput = input("Please enter your password: ")
         print(passline)
         if (passline == passwordinput):
+            #correct input
+            #begin processing the rest of the functions
             print("Password match")
+            #open the employee database to find user name, position and hours worked.
+            with open("C:/Users/davisgj/Desktop/Python Files/Employee Time Project/EmployeeProject/Employees.txt", "r") as employeedatabase:
+                #do the thing
+                print("-----begin new section------")
+                print()
+                lines = employeedatabase.readlines()
+                for line in lines:
+                    print(line)
+                    if(line.__contains__(currentusername)):
+                        currentdataline = line
+                print("the employee's name is: ", currentdataline[0:10])
+                print("The Employees position is: ", currentdataline[11:20])
+                print("The Employees Rights are: ", currentdataline[21:30])
+                print("the employees hours are: ", currentdataline[31:])
+                print()
+                employeename = currentdataline[0:10]
+                employeeposition = currentdataline[11:20]
+                employeerights = currentdataline[22:30]
+                employeehours = currentdataline[32:]
+                print(":::",employeename,":::")
+                remove(employeename)
+                x = 0
+                while (x < len(employeename)):
+                    print("----", employeename, "----")
+                    if (employeename[x] == " "):
+                        employeename[x].replace(" ","",1)
+                        print(x)
+                        print(employeename[x])
+                    x += 1
+                print("::::", employeename,":::")
+                print(":::",employeeposition,":::")
+                print(":::",employeerights,":::")
+                print(":::",employeehours,":::")
+                print("-----end new section-----")
+            print(currentusername)
+
         else:
             print("Password doesn't match")
 
